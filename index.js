5e0223c2498056a9383eb00dd90e7db8bec08704
Matt Raines <mraines4@gmail.com>
2019-04-01 10:50:29 -0400

    adds route parameters example

--- index.js
+++ index.js
@@ -9,21 +9,38 @@ const app = express();
 // respond to GET requests for the path "/"
 app.get('/', (req, res) => {
     // note: this is different from the plain `res.end`
+    console.log('sending the home page');
     res.send('Home Page');
 });
 
 app.post('/', (req, res) => {
+    console.log('responding to a post');
     res.send('you sent a POST');
 });
 
 app.put('/', (req, res) => {
+    console.log('responding to a put');
     res.send('you sent a PUT');
 });
 
+// 1. identify the variable name that you want to have
+// 2. in the string, write that variable name as part of the path
+// 3. put a colon in front of the variable name in the path
+// 4. it will now be part of req.params
+app.put('/users/:userID', (req, res) => {
+    console.log(req.params.userID);
+    res.send(`you sent me a put for ${req.params.userID}`);
+});
+
 app.delete('/', (req, res) => {
+    console.log('responding to a delete');
     res.send('you sent a DELETE');
 });
 
+app.delete('/users/:userID/delete', (req, res) => {
+    console.log(req.params.userID);
+    res.send(`you just deleted ${req.params.userID}`);
+});
 
 app.get('/bye', (req, res) => {
     res.send('BYE YALL')
